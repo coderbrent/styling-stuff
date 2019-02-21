52f31f919b3573037bf09cd324bebbf1a70f6f54
Brent Abruzese <brent.abruzese@gmail.com>
2019-02-21 16:00:30 -0500

    Added base files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+    let text_center = () => {
+        document.getElementById("col_1").style.textalign = "center";
+    }
+
+    let text_right = () => {
+        document.getElementById("row_1").style="text-align: right";
+    }
+
+    function text_left() {
+        document.getElementById("col_1").style="text-align: left";
+    }
